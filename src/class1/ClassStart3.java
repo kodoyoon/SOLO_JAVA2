0777a6ebe26de77d04518886cfcc70b00842c4fa
doyoon <doyoun0527@naver.com>
2023-12-15 20:33:47 +0900

    클래스 도입

--- src/class1/ClassStart3.java
+++ src/class1/ClassStart3.java
@@ -4,10 +4,10 @@ public class ClassStart3 {
 
   public static void main(String[] args) {
     Student student1; //학생을 담을 변수를 선언, Student 타입을 받을 수 있는 변수를 선언
-    student1 = new Student(); //Student 클래스 정보를 기반으로 새로운 객체를 생성하라는 뜻이다.
-    student1.name = "학생1"; // . 은 클래스로 접근하는 방법, //x001
+    student1 = new Student(); //Student 클래스 정보를 기반으로 새로운 객체를 생성하라는 뜻이다. , student1 이 포스트잇, 변수
+    student1.name = "학생1"; // . 은 인스턴스로 접근하는 방법, //x001
     student1.age = 15;
-    student1.grade = 90; //객체 내부의 변수
+    student1.grade = 90; //객체 내부의 변수 //stduent1 이 인스턴스 왜냐하면 인스턴스를 담아서
 
     Student student2 = new Student();  //Student = student2
     //------------------------------- // student2 = new Student2;
--- src/class1/Student.java
+++ src/class1/Student.java
@@ -5,3 +5,4 @@ public class Student { //클래스는 대문자로 시작하고 낙타 표기법
   int age; //변수
   int grade; //변수
 }
+//클래스에 정의한 변수들을 멤버 변수, 또는 필드
